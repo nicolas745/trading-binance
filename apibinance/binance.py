0dbe2dce7d413dacb3dcf7e621e3ca55fcba394d
nicolas <47307856+nicolas745@users.noreply.github.com>
2024-02-20 18:08:07 +0100

    d

--- apibinance/binance.py
+++ apibinance/binance.py
@@ -17,7 +17,6 @@ class Binance:
             self.api_secret = os.getenv(configenv.API_SECRET.value)
             self.api_key = os.getenv(configenv.API_KEY.value)
         self.client = Client(self.api_key , self.api_secret,testnet=self.testnet)
-        Client.transfer_history()
     def get_spot(self) -> spot:
         return spot(self.client)
     def get_earn(self):
--- apibinance/stream.py
+++ apibinance/stream.py
@@ -1,22 +1,24 @@
-from binance.client import Client
+from binance.client import Client ,AsyncClient
 from binance.streams import BinanceSocketManager
 import time
+import asyncio
 import threading
 import os
 from classenum.env import configenv
 class stream:
     def __init__(self, client:Client)  -> None:
         self.client = client
-        threading.Thread(target=self.prix).start()
-    async def start(self):
-        client = Client("api_key", "api_secret")
+        threading.Thread(target=self.start).start()
+    def start(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.updateprix())
+    def getdata(self,data):
+        print(data)
+    async def updateprix(self):
+        client = await AsyncClient.create(api_key=self.client.API_KEY, api_secret=self.client.API_SECRET)
         bm = BinanceSocketManager(client)
-        ts = bm.trade_socket("{}{}".format(os.getenv(configenv.MONEY_PTINCIPAL.value),os.getenv(configenv.MONEY_ECHANGE)))
+        ts = bm.futures_socket()  # Vous pouvez également essayer bm.futures_user_socket()
         async with ts as tscm:
-            while(True):
-                time.sleep(1)
+            while True:
                 res = await tscm.recv()
                 print(res)
-    def update(self,data):
-        print(data)
-
