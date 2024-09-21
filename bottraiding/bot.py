15f96fd316b3bf11a3cad7cddcca50f7fa72fbf4
nicolas <47307856+nicolas745@users.noreply.github.com>
2024-09-21 20:44:19 +0200

    add indicateur

--- bottraiding/bot.py
+++ bottraiding/bot.py
@@ -10,6 +10,7 @@ from apibinance.spot import spot
 from apibinance.simpleearn import simple_earn
 from datetime import datetime, timedelta
 from .order import orders as buysell
+import json
 res = True
 class bot:
     def __init__(self, db:TradingDatabase,client:AsyncClient) -> None:
@@ -20,30 +21,96 @@ class bot:
         self.date = enumsql.DATE.value
         self.res = True
         self.sellprix = 0
+        self.filejson =  "jsondate.json"
         self.time = 0
+    def verifier_ou_creer_fichier(self,fichier):
+        # Vérifier si le fichier existe
+        if not os.path.exists(fichier):
+            # Créer un fichier JSON vide
+            with open(fichier, 'w') as f:
+                json.dump([], f, indent=4)
+    def condition_ma(self,fichier):
+        if os.path.exists(fichier):
+            with open(fichier, 'r') as f:
+                donnees = json.load(f)
+                prix = [d['prix'] for d in donnees]
+
+                if len(prix) >= 200:
+                    # Calcul de MA100 et MA200
+                    ma100 = np.mean(prix[-100:])
+                    ma200 = np.mean(prix[-200:])
+                    difference = ma100 - ma200
+
+                    # Calcul de la dérivée de la différence
+                    if len(prix) > 200:
+                        ma100_past = np.mean(prix[-101:-1])
+                        ma200_past = np.mean(prix[-201:-1])
+                        difference_past = ma100_past - ma200_past
+                        derivee_difference = difference - difference_past
+                    else:
+                        derivee_difference = 0
+
+                    # Vérification des conditions
+                    return difference > 0 or derivee_difference > 0 or derivee_difference == 0
+        return False
+    def date_plus_recente(self, fichier):
+        if os.path.exists(fichier):
+            with open(fichier, 'r') as f:
+                donnees = json.load(f)
+                if donnees:
+                    date_recente = donnees[-1]['date']
+                    date_recente = datetime.strptime(date_recente, '%Y-%m-%d').date()
+                    date_aujourdhui = datetime.now().date()
+
+                    return date_recente == date_aujourdhui
+        return False
+    def ajouter_donnee_json(self,fichier, prix):
+        # Charger les données existantes
+        if os.path.exists(fichier):
+            with open(fichier, 'r') as f:
+                donnees = json.load(f)
+        else:
+            donnees = []
+        nouvelle_donnee = {
+            "date": datetime.now().strftime('%Y-%m-%d'),
+            "prix": prix
+        }
+        donnees.append(nouvelle_donnee)
+
+        # Limiter à 300 enregistrements
+        if len(donnees) > 300:
+            donnees.pop(0)
+
+        # Sauvegarder dans le fichier
+        with open(fichier, 'w') as f:
+            json.dump(donnees, f, indent=4)
     def start(self, actifprix,Socketio:SocketIO):
+        self.verifier_ou_creer_fichier(fichier=self.filejson)
         executorder=0
         actifprix = float(actifprix)
+        if( not self.date_plus_recente(self.filejson)):
+            self.ajouter_donnee_json(self.filejson,actifprix)
         orders = self.db.get_all_orders()
         if self.sellprix < actifprix/1.01:
             self.sellprix = actifprix/1.01
         elif actifprix <self.sellprix:
             self.sellprix = actifprix
         if orders.__len__():
-            for order in orders:
-                pricipal= order[self.moneyprincipal]
-                actif=order[self.moneyechange]
-                lastdate=datetime.strptime(order[self.date], "%Y-%m-%dT%H:%M").timestamp()
-                nowdate = datetime.now().timestamp()
-                defdate=nowdate-lastdate
-                prix = pricipal/actif
-                newprix = prix * pow(1 + 0.001, 2) * pow(1 + 0.06 / (365 * 24 * 60 * 60), defdate)
-                pourcentage=(pricipal+((float(actifprix)-newprix)*actif))/pricipal-1
-                if 0.011<pourcentage:
-                    if(executorder<4):
-                        executorder+=1
-                        if(actifprix<=self.sellprix):
-                            buysell(self.client).sell(Socketio,order)
+            if not self.condition_ma(self.filejson):
+                for order in orders:
+                    pricipal= order[self.moneyprincipal]
+                    actif=order[self.moneyechange]
+                    lastdate=datetime.strptime(order[self.date], "%Y-%m-%dT%H:%M").timestamp()
+                    nowdate = datetime.now().timestamp()
+                    defdate=nowdate-lastdate
+                    prix = pricipal/actif
+                    newprix = prix * pow(1 + 0.001, 2) * pow(1 + 0.06 / (365 * 24 * 60 * 60), defdate)
+                    pourcentage=(pricipal+((float(actifprix)-newprix)*actif))/pricipal-1
+                    if 0.011<pourcentage:
+                        if(executorder<4):
+                            executorder+=1
+                            if(actifprix<=self.sellprix):
+                                buysell(self.client).sell(Socketio,order)
         user=self.db.get_portfolio_data()
         self.time=(datetime.now().timestamp()-datetime.strptime(user[self.date], "%Y-%m-%dT%H:%M").timestamp())
         if(12*60*60<self.time):
